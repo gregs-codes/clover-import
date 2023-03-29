e26057472ff76a6e371bde7cb44be0106d5c314f
Yegor Shabanov <yegorshabanov@Yegors-MBP-2.lan>
2023-03-28 17:00:17 -0700

    created a kiosk view

--- src/ListCategories.js
+++ src/ListCategories.js
@@ -1,6 +1,6 @@
 import React,{ useState } from 'react';
 import Fetch from './Fetch';
-import ListItems from './ListItems'
+import './Kiosk.css'
 
 function groupItemsByCategoryId(items) {
     return items.reduce((acc, item) => {
@@ -20,29 +20,78 @@ function groupItemsByCategoryId(items) {
 function ListCategories() {
    // const [categoryObj, setCategory] = useState();
     const { data, loading, error } = Fetch("items");
-  
-   // let myArray = {};
-  
+    const [cart, setCart] = useState([]);
+    const [showCart, setShowCart] = useState(false);
+    const handleAddToCart = (item) => {
+      setCart([...cart, item]);
+    };
+    const handleShowCart = () => {
+      setShowCart(!showCart);
+    };
+    const removeFromCart = (item) => {
+      const newCart = [...cart];
+      newCart.splice(item, 1);
+      setCart(newCart);
+    };
+
+   function formatPrice(price) {
+    return price.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 });
+  }
     if (loading === false && data != null) {
       const itemsByCategoryId = groupItemsByCategoryId(data.elements);
       console.log(itemsByCategoryId)
+  
       return (
-        <div>
+        <div className="kiosk-view">
+        <header className="top-menu">
+          <h1 className="logo">My Kiosk</h1>
+          <div className="cart-container" onClick={handleShowCart}>
+            Cart
+            {cart.length > 0 && (
+              <div className="cart-count">{cart.length}</div>
+            )}
+          </div>
+        </header>
+        {showCart && (
+          <div className="cart-dropdown">
+            <h3 className="cart-title">Your Cart</h3>
+            {cart.length > 0 ? (
+              cart.map((item) => (
+                <div key={item.id} className="cart-item">
+                  <img src={item.imageUrl} alt={item.name} className="cart-item-image" />
+                  <div className="cart-item-details">
+                    <h4 className="cart-item-name">{item.name}</h4>
+                    <p className="cart-item-price">{formatPrice(item.price)}</p>
+                    <button onClick={() => removeFromCart(item)}>Remove</button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="cart-empty-message">Your cart is empty</p>
+            )}
+          </div>
+        )}
           {Object.entries(itemsByCategoryId).map(([categoryId, items]) => (
-            <div key={categoryId}>
-             <ul>Category Name: {categoryId}
-
-              {items.map((item) => (
-                <li key={item.id}>
-                  {/* <img width={'100px'} src={`https://dev.cloverstatic.com/menu-assets/items/${item.id}.jpeg`} />*/}
-                  {item.name} - {item.price} 
-                </li>
-              ))}
-              </ul> 
+            <div key={categoryId} className="category">
+              <h2 className="category-heading">{categoryId}</h2>
+              <div className="item-grid">
+                {items.map((item) => (
+                  <div key={item.id} className="item">
+                    <img src={item.imageUrl} alt={item.name} className="item-image" />
+                    <h3 className="item-name">{item.name}</h3>
+                    <p className="item-description">{item.description}</p>
+                    <p className="item-price">{formatPrice(item.price)}</p>
+                    <button onClick={() => handleAddToCart(item)} className="add-to-cart-button">
+                      Add to Cart
+                    </button>
+                </div>
+                ))}
+              </div>
             </div>
           ))}
         </div>
       );
+            
     }
     if (loading) return <h1> LOADING ... </h1>;
     if (error) console.log(error);
